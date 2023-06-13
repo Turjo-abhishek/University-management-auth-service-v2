e8e1f8ab84588606e8f519be714d21c5885e4a55
Turjo-abhishek <turjoabhishek@gmail.com>
2023-06-13 17:12:53 +0600

    setup complete error handling

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import { UserRoutes } from './app/modules/user/user.route';
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/users/', UserRoutes);
+app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+
+//Testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing Error logger')
+// })
+
+//global error handler
+app.use(globalErrorHandler);
+
+export default app;
